0a00bd3591fdbbb6e9a196d8691b1822bda71bc3
Ernesto Pena <ernestopenad@gmail.com>
2019-07-30 14:38:53 -0500

    Initial project setup. Server is running

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-const express = 'express';
+// Defining Express
+const express = require('express');
 
+//Instance of Express
 const server = express();
+
+//Defining a port for server
+const port = 4000;
+
+//Using json for Express
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
+
+
 
 //custom middleware
 
 function logger(req, res, next) {
 
 };
+
+
+
+//Running the server.....
+server.listen(port , () => {
+  console.log(`Server is listening on port ${port}`);
+})
+
+server.listen()
 
 module.exports = server;
